03230a25a65bcd40394976104ef3e38ca818d749
Antoine Rose <antoine.rose@one2team.com>
2018-07-24 13:28:44 -0700

    Edits

--- bible.js
+++ bible.js
@@ -15,19 +15,19 @@
       return user.admin || user.id === owner.id;
     },
   },
-  middleware: {
-    get: function(owner) {
-      owner.pets = this.collections.pets.get().filter(function(pet) {
-        return pet.owners.indexOf(owner.id) !== -1;
-      })
-      delete owner.password;
-      return owner;
-    },
-    'post|put': function(owner) {
-      if (!owner.password) throw new Error("Password must be specified");
-      owner.password = this.utils.hash(owner.password);
-    }
-  },
+  // middleware: {
+  //   get: function(owner) {
+  //     owner.pets = this.collections.pets.get().filter(function(pet) {
+  //       return pet.owners.indexOf(owner.id) !== -1;
+  //     })
+  //     delete owner.password;
+  //     return owner;
+  //   },
+  //   'post|put': function(owner) {
+  //     if (!owner.password) throw new Error("Password must be specified");
+  //     owner.password = this.utils.hash(owner.password);
+  //   }
+  // },
   sort: function(a1, a2) {
     var d1 = new Date(a1.date);
     var d2 = new Date(a2.date);
