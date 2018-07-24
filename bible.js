b355fe4d4ecf03d9d576f7c87d2f62f8366e5d16
Antoine Rose <antoine.rose@one2team.com>
2018-07-23 23:04:51 -0700

    Edits

--- bible.js
+++ bible.js
@@ -1,6 +1,23 @@
 {
+  id: "name",
+  schema: {
+    type: "object",
+    properties: {
+      name: {type: "string"},
+      age: {type: "number"},
+      type: {type: "string"},
+      owners: {type: "array", items: {type: "string"}},
+    }
+    additionalProperties: false,
+  },
+  attachments: {
+    photo: {
+      extension: 'png',
+      strategy: 'link',
+    }
+  },
   access: {
-    get: 'all',
-    post: 'all',
-  }
+    get: "all",
+    post: "all",
+  },
 }
